5a75acd00947e2e8f0641d6a78bc88fb0f81ea56
yuliaokta <114564209+yuliaokta@users.noreply.github.com>
2022-09-27 19:38:18 -1200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace mathoperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Modulus of 200 / 5 : " + (200 % 5));
+Console.WriteLine("Modulus of 7 / 5 : " + (7 % 5));
+        }
+    }
+}
